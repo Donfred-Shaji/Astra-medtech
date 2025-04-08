ccc5b866aaae9e4ecdd4f4f053cd335745337f3a
Donfred-Shaji <136463053+Donfred-Shaji@users.noreply.github.com>
2025-04-08 13:21:02 +0530

    Add files via upload

---
+++ view_arm.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('two_dof_arm')
+    xacro_file = os.path.join(pkg_path, 'urdf', '2dof_arm.xacro')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{
+                'robot_description': Command(['xacro', xacro_file])
+            }]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(pkg_path, 'rviz', 'arm.rviz')],
+            output='screen'
+        )
+    ])
+
